6d3ed560674064c8675c4b409ae19d4041fe15d4
ramiro-sepulveda <162350790+ramiro-sepulveda@users.noreply.github.com>
2024-06-16 00:38:38 -0400

    arreglos

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,16 +5,16 @@ const Navigation = () => {
   return (
     <Navbar bg="danger" variant="dark">
       <Container className="justify-content-start gap-2">
-        <Link to="/desafio-happy-cake/" className="text-white ms-3 text-decoration-none">
+        <Link to="/" className="text-white ms-3 text-decoration-none">
            Home
         </Link>
-        <Link to="/desafio-happy-cake/contacto" className="text-white ms-3 text-decoration-none">
+        <Link to="/contacto" className="text-white ms-3 text-decoration-none">
            Contacto
         </Link>
       </Container>
       <Container className=" justify-content-end">
         <Navbar.Brand>
-          <Link to="/desafio-happy-cake/" className="text-white ms-3 text-decoration-none">
+          <Link to="/" className="text-white ms-3 text-decoration-none">
             Happy Cake 
           </Link>
         </Navbar.Brand>
